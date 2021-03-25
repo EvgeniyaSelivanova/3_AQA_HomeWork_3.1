7cd3a5aed7369aa55e2e55201c179cecfda190fa
Evgeniya Selivanova <kulbatskaya.evgenia@gmail.com>
2021-03-25 21:21:27 +0300

    Added negotive tests in branch negotive

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -3,11 +3,12 @@ package ru.netology;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class CallbackTest {
 
@@ -78,4 +79,45 @@ public class CallbackTest {
         $("[data-test-id='order-success']").shouldHave(exactText(" Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
+    @Test
+    void shouldTestEnglishWordsInName() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[class='form form_size_m form_theme_alfa-on-white']");
+        form.$("[data-test-id='name'] input").setValue("Vasiliy");
+        form.$("[data-test-id='phone'] input").setValue("+79990001122");
+        form.$("[data-test-id='agreement']").click();
+        form.$("[type='button']").click();
+        $("[class*='input_invalid']").should(exist);
+    }
+
+    @Test
+    void shouldTestEmptyName() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[class='form form_size_m form_theme_alfa-on-white']");
+        form.$("[data-test-id='phone'] input").setValue("+79990001122");
+        form.$("[data-test-id='agreement']").click();
+        form.$("[type='button']").click();
+        $("[class*='input_invalid']").should(exist);
+    }
+
+    @Test
+    void shouldTestEmptyPhone() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[class='form form_size_m form_theme_alfa-on-white']");
+        form.$("[data-test-id='name'] input").setValue("Василий");
+        form.$("[data-test-id='agreement']").click();
+        form.$("[type='button']").click();
+        $("[class*='input_invalid']").should(exist);
+    }
+
+    @Test
+    void shouldTestInvalidPhoneNumber() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[class='form form_size_m form_theme_alfa-on-white']");
+        form.$("[data-test-id='name'] input").setValue("Василий");
+        form.$("[data-test-id='phone'] input").setValue("79990001122");
+        form.$("[data-test-id='agreement']").click();
+        form.$("[type='button']").click();
+        $("[class*='input_invalid']").should(exist);
+    }
 }
