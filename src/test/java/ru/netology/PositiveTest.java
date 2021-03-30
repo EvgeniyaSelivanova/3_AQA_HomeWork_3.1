30d8cd3c4582fc05ff3d2339628a2b910aeeb72e
Evgeniya Selivanova <kulbatskaya.evgenia@gmail.com>
2021-03-30 22:25:17 +0300

    Added negotive class in branch master

--- src/test/java/ru/netology/PositiveTest.java
+++ src/test/java/ru/netology/PositiveTest.java
@@ -1,16 +1,13 @@
 package ru.netology;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CallbackTest {
-
+public class PositiveTest {
 
     @Test
     void shouldTestSomething() {
@@ -35,10 +32,10 @@ public class CallbackTest {
     }
 
     @Test
-    void shouldTestTwoWordsWithDashInName() {
+    void shouldTestTwoWordsWithDashInSurname() {
         open("http://localhost:9999");
         SelenideElement form = $("[class='form form_size_m form_theme_alfa-on-white']");
-        form.$("[data-test-id='name'] input").setValue("Иванов-Василий");
+        form.$("[data-test-id='name'] input").setValue("Иванов-Петров Василий");
         form.$("[data-test-id='phone'] input").setValue("+79990001122");
         form.$("[data-test-id='agreement']").click();
         form.$("[type='button']").click();
@@ -46,21 +43,11 @@ public class CallbackTest {
     }
 
     @Test
-    void shouldTestTwoSmallWordsInName() {
+    void shouldTestSmallAndBigWordsInName() {
         open("http://localhost:9999");
         SelenideElement form = $("[class='form form_size_m form_theme_alfa-on-white']");
-        form.$("[data-test-id='name'] input").setValue("иванов василий");
-        form.$("[data-test-id='phone'] input").setValue("+79990001122");
-        form.$("[data-test-id='agreement']").click();
-        form.$("[type='button']").click();
-        $("[data-test-id='order-success']").shouldHave(exactText(" Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
-    }
-
-    @Test
-    void shouldTestTwoBigWordsInName() {
-        open("http://localhost:9999");
-        SelenideElement form = $("[class='form form_size_m form_theme_alfa-on-white']");
-        form.$("[data-test-id='name'] input").setValue("ИВАНОВ ВАСИЛИЙ");
+//        султан Занзибара в 1890—1893 годах
+        form.$("[data-test-id='name'] input").setValue("Али ибн Саид");
         form.$("[data-test-id='phone'] input").setValue("+79990001122");
         form.$("[data-test-id='agreement']").click();
         form.$("[type='button']").click();
@@ -71,7 +58,8 @@ public class CallbackTest {
     void shouldTestFourSmallAndBigWordsWithDashInName() {
         open("http://localhost:9999");
         SelenideElement form = $("[class='form form_size_m form_theme_alfa-on-white']");
-        form.$("[data-test-id='name'] input").setValue("ИВАНОВ ВАСИЛИЙ иванов-василий");
+//        Султан Египта и Хиджаза, мамлюк абхазского происхождения.
+        form.$("[data-test-id='name'] input").setValue("Али-бей аль-Кабир");
         form.$("[data-test-id='phone'] input").setValue("+79990001122");
         form.$("[data-test-id='agreement']").click();
         form.$("[type='button']").click();
